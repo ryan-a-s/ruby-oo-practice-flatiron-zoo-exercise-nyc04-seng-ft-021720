4b71e0aade810623dc8ffb50d49b6a4d8b6f5b7a
Ryan Seit <ryan@ryanseit.com>
2020-02-26 08:24:12 -0500

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -10,6 +10,10 @@ class Zoo
         @@all << self
     end
 
+    def name
+        @name
+    end
+
     # should return an array of all the zoo instances
     def self.all
         @@all
@@ -23,19 +27,20 @@ class Zoo
     # should return an array of all the species (as strings) of the animals in the zoo
         # However,if you have two dogs, it should only return one "Dog" string (aka an **unique** array)
     def animal_species
-        self.animals.map {|animal| animal.species}.uniq
+        animals.map {|animal| animal.species}.uniq
     end
 
     # should take in an animal's species as an argument and return an array of all the 
     # animals in that zoo, which are of that species.
     def find_by_species(species)
-        Animal.all.select {|animal| animal.species == species && animal.zoo == self}
+        animals.select {|animal| animal.species}
     end
 
     # should return an array of all the nicknames of animals that a specific instance of a zoo has
     def animal_nicknames
-       animals = Animal.all.select {|animal| animal.nickname && animal.zoo == self}
-       animals.map {|animal| animal.nickname}
+    #    animals = 
+       animals.select {|animal| animal.nickname}
+    #    animals.map {|animal| animal.nickname}
     end
 
     # should take in a location as an argument and return an array of all the zoos within that location.
